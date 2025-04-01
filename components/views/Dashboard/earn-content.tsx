959f1fa29ad6fd37d4439a5826fcf6287f0c3e9c
Night <hotcoding85@gmail.com>
2025-04-01 11:43:35 -0400

    feat: implemented theme toggle & fixing UI issues with language bar

--- components/views/Dashboard/earn-content.tsx
+++ components/views/Dashboard/earn-content.tsx
@@ -12,6 +12,7 @@ import { useMemo, useState } from "react";
 import { useLanguage } from "@/contexts/language-context";
 import { vaults } from "@/lib/data";
 import Link from "next/link";
+// import { HowEarnWorks } from "./how-earn-works";
 
 const initialColumns = [
   { id: "vaultName", title: "Vault Name", visible: true },
@@ -31,7 +32,7 @@ export function EarnContent() {
   const [searchQuery, setSearchQuery] = useState("");
   const [sortColumn, setSortColumn] = useState<string>("vaultName");
   const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
-  const [, setShowHowEarnWorks] = useState(false);
+  // const [showHowEarnWorks, setShowHowEarnWorks] = useState(false);
 
   // Function to handle sorting
   const handleSort = (columnId: string, direction: "asc" | "desc") => {
@@ -129,29 +130,27 @@ export function EarnContent() {
       // translatedTitle: t(column.title),
     }));
 
-
-
   return (
     <div className="space-y-6">
       <div className="flex flex-row justify-between">
-        <h1 className="text-[38px] text-white flex items-center">
+        <h1 className="text-[38px] text-primary flex items-center">
           {t("common.earn")}
         </h1>
         <div className="hidden gap-3 md:flex">
           <Link href={"./analytics"}>
-            <Card className="bg-[#202426] gap-5 border-none cursor-pointer p-5 flex flex-col h-[98px] min-w-[194px] rounded-[12px]">
+            <Card className="bg-foreground gap-5 border-none cursor-pointer p-5 flex flex-col h-[98px] min-w-[194px] rounded-[12px]">
               <CardHeader className="flex flex-row items-center justify-between p-0 w-full">
                 <CardTitle className="text-sm font-medium text-muted-foreground">
                   <div className="flex items-center gap-2 w-full">
                     <Deposit className="h-[14px] w-[14px]" />
-                    <div className="text-[#ffffffcc] text-[12px]">
+                    <div className="text-secondary text-[12px]">
                       {t("common.totalDeposits")}
                     </div>
                   </div>
                 </CardTitle>
               </CardHeader>
               <CardContent className="p-0 h-[20px]">
-                <div className="font-normal text-white text-[15px] pb-2">
+                <div className="font-normal text-secondary text-[15px] pb-2">
                   $4,736,811,455
                 </div>
               </CardContent>
@@ -159,19 +158,19 @@ export function EarnContent() {
           </Link>
 
           <Link href={"./analytics"}>
-            <Card className="bg-[#202426] gap-5 border-none cursor-pointer p-5 flex flex-col h-[98px] min-w-[194px] rounded-[12px]">
+            <Card className="bg-foreground gap-5 border-none cursor-pointer p-5 flex flex-col h-[98px] min-w-[194px] rounded-[12px]">
               <CardHeader className="flex flex-row items-center justify-between p-0 w-full">
                 <CardTitle className="text-sm font-medium text-muted-foreground">
                   <div className="flex items-center gap-2">
                     <Borrow className="h-[14px] w-[14px]" />
-                    <div className="text-[#ffffffcc] text-[12px]">
+                    <div className="text-secondary text-[12px]">
                       {t("common.totalBorrow")}
                     </div>
                   </div>
                 </CardTitle>
               </CardHeader>
               <CardContent className="p-0 h-[20px]">
-                <div className="text-[15px] font-normal text-white mb-2">
+                <div className="text-[15px] font-normal text-secondary mb-2">
                   $1,706,255,566
                 </div>
               </CardContent>
@@ -183,13 +182,13 @@ export function EarnContent() {
       <div className="space-y-4 mt-20">
         <div className="flex gap-4 md:items-center md:justify-between flex-col md:flex-row">
           <div className="flex items-center gap-4">
-            <h2 className="text-[16px] font-normal text-[#fffffff2]">
+            <h2 className="text-[16px] font-normal text-card">
               {t("common.depositInVault")}
             </h2>
             <CustomButton
               variant="secondary"
               className="h-auto text-[11px] rounded-[2px]"
-              onClick={() => setShowHowEarnWorks(true)}
+              // onClick={() => setShowHowEarnWorks(true)}
             >
               {t("common.howDoesItWork")}
             </CustomButton>
@@ -205,7 +204,7 @@ export function EarnContent() {
               <Input
                 type="search"
                 placeholder={t("common.searchVaults")}
-                className="pl-8 text-xs h-[32px] md:w-[150px] text-white border-[#afafaf1a] focus:border-[#afafaf1a] focus:border-none"
+                className="pl-8 text-xs h-[32px] md:w-[150px] text-primary border-[#afafaf1a] focus:border-[#afafaf1a] focus:border-none"
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
               />
