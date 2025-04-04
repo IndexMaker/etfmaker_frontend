0fbb3f92032cfd48371a265f7df9706d086a40ba
Night <hotcoding85@gmail.com>
2025-04-04 10:39:50 -0400

    feat: integrated blockweb & fixed UI issues

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -2,22 +2,46 @@
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { Maximize2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { NetworkSwitcher } from "../elements/network-switcher";
 import { CustomButton } from "../ui/custom-button";
 import Navigation from "../icons/navigation";
 import { LanguageSelector } from "../elements/language-selector";
 import { useLanguage } from "@/contexts/language-context";
+import onboard from "@/lib/blocknative/web3-onboard";
+import { useDispatch, useSelector } from "react-redux";
+import { clearWallet, setWallet } from "@/redux/walletSlice";
+import { shortenAddress } from "@/lib/utils";
+import { RootState } from "@/redux/store";
+import NavigationAlert from "../icons/navigation-alert";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@radix-ui/react-popover";
+import RightArrow from "../icons/right-arrow";
+import Switch from "../icons/switch";
+import Disconnect from "../icons/disconnect";
+import { useCallback, useEffect, useState } from "react";
 
 interface HeaderProps {
   sidebarOpen: boolean;
   setSidebarOpen: (open: boolean) => void;
+  rightbarOpen: boolean;
+  setRightbarOpen: (open: boolean) => void;
 }
 
-export function Header({ sidebarOpen, setSidebarOpen }: HeaderProps) {
-  const { t } = useLanguage()
+export function Header({
+  sidebarOpen,
+  setSidebarOpen,
+  rightbarOpen,
+  setRightbarOpen,
+}: HeaderProps) {
+  const { t } = useLanguage();
   const pathname = usePathname();
+  const [switchOption, setSwitchOption] = useState<boolean>(false);
+  const dispatch = useDispatch();
+  const storedWallet = useSelector((state: RootState) => state.wallet.wallet);
 
   // Generate breadcrumb items
   const pathSegments = pathname.split("/").filter((segment) => segment);
@@ -27,12 +51,51 @@ export function Header({ sidebarOpen, setSidebarOpen }: HeaderProps) {
 
   const breadcrumbItems = pathSegments.map((segment, index) => {
     const path = `../${pathSegments.slice(0, index + 1).join("/")}`;
-    const _segment = t('common.' + segment)
+    const _segment = t("common." + segment);
     return {
       name: _segment.charAt(0).toUpperCase() + segment.slice(1), // Capitalize first letter
       href: path,
     };
   });
+
+  useEffect(() => {
+    if (!storedWallet && switchOption) {
+      connectWallet();
+      setSwitchOption(false);
+    }
+  }, [storedWallet, switchOption, setSwitchOption]);
+
+  const connectWallet = useCallback(async () => {
+    if (storedWallet) {
+      console.log("Wallet already connected:", storedWallet);
+      return;
+    }
+
+    const wallets = await onboard.connectWallet();
+    if (wallets.length > 0) {
+      const { label, accounts, chains, icon } = wallets[0]; // Extract only serializable data
+      console.log("Connected Wallet:", wallets[0]);
+
+      dispatch(setWallet({ label, accounts, chains, icon })); // Store only serializable parts
+
+      // hide onboard-v2 elements...
+    }
+  }, [storedWallet, dispatch]);
+
+  const disconnectWallet = async () => {
+    if (storedWallet) {
+      await onboard.disconnectWallet({ label: storedWallet.label });
+      dispatch(clearWallet()); // Clear from Redux
+    }
+  };
+
+  const switchWallet = useCallback(async () => {
+    setSwitchOption(true);
+    if (storedWallet) {
+      await onboard.disconnectWallet({ label: storedWallet.label });
+      dispatch(clearWallet()); // Clear from Redux
+    }
+  }, [storedWallet, dispatch]);
 
   return (
     <header className="flex h-[55px] md:h-[50px] pt-0 shrink-0 items-center border-b border-transparent bg-background px-[11px] lg:px-[40px]">
@@ -42,25 +105,30 @@ export function Header({ sidebarOpen, setSidebarOpen }: HeaderProps) {
         className="lg:hidden"
         onClick={() => setSidebarOpen(!sidebarOpen)}
       >
-        <Navigation className="h-6 w-6 text-white" />
+        <Navigation className="h-6 w-6 text-primary" />
         <span className="sr-only">Toggle menu</span>
       </Button>
 
       {/* Breadcrumb (only if there's a second segment) */}
       {shouldShowBreadcrumb && (
-        <nav className="text-sm text-gray-400 hidden md:flex">
+        <nav className="text-sm text-secondary hidden md:flex">
           <ol className="flex items-center space-x-2">
             {breadcrumbItems.map((item, index) => (
               <li key={item.href} className="flex items-center">
                 {index === breadcrumbItems.length - 1 ? (
-                  <span className="text-muted text-semibold text-[13px]">{item.name}</span>
+                  <span className="text-muted text-semibold text-[13px]">
+                    {item.name}
+                  </span>
                 ) : (
                   <Link
                     href={item.href}
                     className="text-muted text-semibold hover:text-primary text-[13px]"
                   >
                     {item.name}
-                    <span className="mx-2 text-muted text-semibold text-[13px]"> / </span>
+                    <span className="mx-2 text-muted text-semibold text-[13px]">
+                      {" "}
+                      /{" "}
+                    </span>
                   </Link>
                 )}
               </li>
@@ -69,17 +137,82 @@ export function Header({ sidebarOpen, setSidebarOpen }: HeaderProps) {
         </nav>
       )}
 
-      <div className="lg:hidden">
-        <Maximize2 className="h-6 w-6 text-primary" />
-      </div>
-
       <div className="ml-auto flex items-center gap-2">
         <LanguageSelector />
         <NetworkSwitcher />
 
-        <CustomButton className="bg-[#2470ff] hover:bg-blue-700 text-[11px] rounded-[3px] cursor-pointer">
-          {t("common.connectWallet")}
-        </CustomButton>
+        {/* Connect Wallet Button */}
+        {storedWallet ? (
+          <Popover>
+            <PopoverTrigger asChild>
+              <CustomButton className="flex items-center gap-1 bg-transparent lg:bg-foreground text-[11px] rounded-[3px] cursor-pointer hover:bg-accent">
+                <div className="w-[17px] h-[17px] rounded-full bg-gradient-to-br from-[#A5FECA] via-[#3EDCEB] via-[#2594FF] to-[#53F]"></div>
+                <span className="text-secondary hidden lg:flex">
+                  {shortenAddress(storedWallet.accounts[0].address)}
+                </span>
+              </CustomButton>
+            </PopoverTrigger>
+            <PopoverContent
+              className="w-[300px] p-0 bg-foreground text-card flex flex-col shadow-[0px_1px_20px_0px_rgba(0,0,0,0.04),0px_12px_16px_0px_rgba(6,9,11,0.05),0px_6px_12px_0px_rgba(0,0,0,0.07)] z-100"
+              align="end"
+              sideOffset={5}
+            >
+              <Link
+                href={
+                  "https://etherscan.io/address/" +
+                  storedWallet.accounts[0].address
+                }
+                target="_blank"
+                className="flex gap-2 px-[8px] py-[12px] items-center h-[48px] border-b-[1px] border-accent cursor-pointer hover:bg-accent"
+              >
+                <div className="w-[17px] h-[17px] rounded-full bg-gradient-to-br from-[#A5FECA] via-[#3EDCEB] via-[#2594FF] to-[#53F]"></div>
+                <span className="text-secondary text-[14px] underline">
+                  {shortenAddress(storedWallet.accounts[0].address)}
+                </span>
+                <RightArrow
+                  className="w-4 h-4 rotate-135"
+                  width="12px"
+                  height="12px"
+                />
+              </Link>
+              <div
+                className="flex gap-2 p-[6px] items-center h-[36px] border-b-[1px] border-accent cursor-pointer hover:bg-accent"
+                onClick={switchWallet}
+              >
+                <Switch className="w-4 h-4 text-primary" />
+                <span className="text-secondary text-[14px]">
+                  {t("common.switchWallet")}
+                </span>
+              </div>
+              <div
+                className="flex gap-2 p-[6px] items-center h-[36px] cursor-pointer hover:bg-accent"
+                onClick={disconnectWallet}
+              >
+                <Disconnect className="w-4 h-4 text-primary" />
+                <span className="text-secondary text-[14px]">
+                  {t("common.disconnectWallet")}
+                </span>
+              </div>
+            </PopoverContent>
+          </Popover>
+        ) : (
+          <CustomButton
+            onClick={connectWallet}
+            className="bg-[#2470ff] hover:bg-blue-700 text-[11px] rounded-[3px] cursor-pointer"
+          >
+            {t("common.connectWallet")}
+          </CustomButton>
+        )}
+
+        <Button
+          variant="ghost"
+          size="icon"
+          className="lg:hidden"
+          onClick={() => setRightbarOpen(!rightbarOpen)}
+        >
+          <NavigationAlert className="h-7 w-7 text-primary" />
+          <span className="sr-only">Toggle Right</span>
+        </Button>
       </div>
     </header>
   );
